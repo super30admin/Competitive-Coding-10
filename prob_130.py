7876edbd721ebd0c68fa4e8a6c5d75ff3e631adf
pnandini <44413220+pnandini@users.noreply.github.com>
2019-12-23 18:07:58 -0800

    Add files via upload

---
+++ prob_130.py
@@ -0,0 +1,113 @@
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iterator = iterator
+        if self.iterator.hasNext():
+            self.current = self.iterator.next()
+        else:
+            self.current = None
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self.current
+
+    def next(self):
+        """
+        :rtype: int
+        """
+
+        if self.iterator.hasNext():
+            self.current, temp = self.iterator.next(), self.current
+            return temp
+        else:
+            self.current, temp = None, self.current
+            return temp
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if not (self.current):
+            return False
+        else:
+            return True
+
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iterator = iterator
+        self.queue = []
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        if not (self.queue):
+            self.queue.append(self.iterator.next())
+            return self.queue[0]
+        else:
+            # self.queue.pop(0)
+
+            return self.queue[0]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if not (self.queue):
+            return self.iterator.next()
+        else:
+            return self.queue.pop(0)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if not (self.queue):
+            return self.iterator.hasNext()
+        else:
+            return True
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].
