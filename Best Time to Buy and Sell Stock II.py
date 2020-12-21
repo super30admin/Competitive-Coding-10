d7afe374ffd65ab8ee2912b5861e445874ab0ac4
Utsav Raychaudhuri <agileutsav@gmail.com>
2020-12-21 03:40:43 -0800

    Solutions for Competitive Coding 10

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,10 @@
+# Time:- O(n)
+# Space:- O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res=0
+        for i in range(1,len(prices)):
+            if prices[i]>prices[i-1]:
+                res+=prices[i]-prices[i-1]
+        return res
+    
