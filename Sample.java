fd3da190c38958ec02b36e243934c149aa2b45d4
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-08-01 11:24:11 -0400

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int slow=0;
+        int fast=1;
+        int max=0;
+        int profit=0;
+        while(fast<prices.length)
+        {
+            if(prices[slow]>prices[fast])
+            {
+                //No profit is generated
+                slow++;
+                fast++;
+            }
+            
+            else if(prices[slow]<=prices[fast])
+            {
+                profit+=prices[fast]-prices[slow];
+                
+                slow++;
+                fast++;
+            }
+        }
+        return profit;
+    }
+}
