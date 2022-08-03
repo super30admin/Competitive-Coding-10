172e11f23423a3abc4c9e40f416f5cd6602d33b3
Vineet Khanna <67041692+vineetvk19@users.noreply.github.com>
2022-08-03 04:27:33 -0400

    Completed maxProfit Problem

---
+++ problem2.py
@@ -0,0 +1,14 @@
+# Time-Complexity: O(n)
+# Space Complexity: O(1)
+# Leetcode status: Accepted
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        result = 0
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                result += prices[i] - prices[i-1]
+        return result
