218ce86a0d916d3311dffd97e1b17c0806a82afe
Fnu Butul Parveen <89315756+Butulparveen@users.noreply.github.com>
2022-08-29 23:07:00 -0700

    Completed the Buy and Sell, Peeking Iterator

---
+++ BuyandSell.java
@@ -0,0 +1,23 @@
+//TC:O(n)
+//SC:O(1)
+//Google
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length ==0) return 0;
+        int i=0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxProfit = 0;
+        while(i<prices.length-1){
+            while(i<prices.length-1 && prices[i]>=prices[i+1])
+            i++;
+            valley = prices[i];
+            while(i<prices.length-1 && prices[i] <= prices[i+1])
+            i++;
+            peak = prices[i];
+            maxProfit+= peak-valley;
+        }
+        return maxProfit;
+    }
+}
